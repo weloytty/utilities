df9bd9dc627103ae44e2532316f7a6c776df0041
Bill Loytty <bill@loytty.com>
2019-08-26 02:47:20 -0400

    enumerate com ports

---
+++ ComPortControl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ComPortControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (var s in System.IO.Ports.SerialPort.GetPortNames()) {
+                Console.WriteLine(s);
+            } 
+        }
+    }
+}
