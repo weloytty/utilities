1888ea1f9b448e3a02960d9a84ea814b5d131d3b
Bill Loytty <loyttybe@corning.com>
2020-09-01 10:42:20 -0400

    Add Timeit

---
+++ timeit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace timeit {
+    class Program {
+        static void Main(string[] args) {
+
+            var fileName = args[0];
+            if (!File.Exists(fileName)) {
+                Console.WriteLine($"Can't find {fileName}");
+                return;
+            }
+
+            ProcessCreator.CreateProcess(0, args[0]);
+        }
+    }
+}
