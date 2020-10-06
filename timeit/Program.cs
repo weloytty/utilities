3c84b647bdcb8a5927b52fb90d112e9ca2a960f9
Bill Loytty <loyttybe@corning.com>
2020-10-06 12:21:11 -0400

    Testing gitversion

--- timeit/Program.cs
+++ timeit/Program.cs
@@ -9,7 +9,9 @@ using System.IO;
 namespace timeit {
     class Program {
         static void Main(string[] args) {
-
+/*
+I am messing around with gitversion
+*/
             var fileName = args[0];
             if (!File.Exists(fileName)) {
                 Console.WriteLine($"Can't find {fileName}");
