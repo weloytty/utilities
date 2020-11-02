d771c6bb33e4250448196bb58c2c60d93cbeaf96
Bill Loytty <bill@loytty.com>
2020-11-02 16:30:49 -0500

    Fixing .vscode in .gitignore and adding some junk

---
+++ asyncmsmqtest/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncmsmqtest {
+    class Program {
+        // Define static class members.
+        static ManualResetEvent signal = new ManualResetEvent(false);
+        static int count = 0;
+
+        //**************************************************
+        // Provides an entry point into the application.
+        //		
+        // This example performs asynchronous receive
+        // operation processing.
+        //**************************************************
+
+        public static void Main() {
+            // Create an instance of MessageQueue. Set its formatter.
+            MessageQueue myQueue = new MessageQueue(".\\private$\\testtransactional");
+            myQueue.Formatter = new XmlMessageFormatter(new Type[]
+                {typeof(String)});
+
+            // Add an event handler for the ReceiveCompleted event.
+            myQueue.ReceiveCompleted +=
+                new ReceiveCompletedEventHandler(MyReceiveCompleted);
+
+            // Begin the asynchronous receive operation.
+            myQueue.BeginReceive();
+
+            //signal.WaitOne();
+
+            // Do other work on the current thread.
+
+            return;
+        }
+
+        //***************************************************
+        // Provides an event handler for the ReceiveCompleted
+        // event.
+        //***************************************************
+
+        private static void MyReceiveCompleted(Object source,
+            ReceiveCompletedEventArgs asyncResult) {
+            try {
+                // Connect to the queue.
+                MessageQueue mq = (MessageQueue)source;
+
+                // End the asynchronous receive operation.
+                Message m = mq.EndReceive(asyncResult.AsyncResult);
+
+                count += 1;
+                if (count == 10) {
+                    signal.Set();
+                }
+
+                // Restart the asynchronous receive operation.
+                mq.BeginReceive();
+            } catch (MessageQueueException) {
+                // Handle sources of MessageQueueException.
+            }
+
+            // Handle other exceptions.
+
+            return;
+        }
+    }
+}
